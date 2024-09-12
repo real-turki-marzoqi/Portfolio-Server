cbe93d612601f223f81bd520fc9ce34415680c4c
real-turki-marzoqi <turki.marzoqi.personal@gmail.com>
2024-09-12 11:40:58 +0300

    first commit

--- utils/validators/personalInfoValidator.js
+++ utils/validators/personalInfoValidator.js
@@ -29,21 +29,6 @@ exports.updatePersonalInfoValidator = [
     check('id')
     .isMongoId().withMessage('Invalid Personal Info Id'),
 
-    check('name')
-        .notEmpty().withMessage('The name is required')
-        .isLength({ min: 3 }).withMessage("Name must be at least 3 characters")
-        .isLength({ max: 50 }).withMessage("Name must be at max 50 characters"),
-
-    // Validate email
-    check('email')
-        .notEmpty().withMessage('Email is required')
-        .isEmail().withMessage('Invalid email format')
-        .trim()
-        .toLowerCase(),
-
-    // Validate number
-    check('number')
-        .notEmpty().withMessage('The number is required')
 
         ,validatorMiddleWare
 
